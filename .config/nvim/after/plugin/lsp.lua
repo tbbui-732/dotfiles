5c72fd200c0235c46579711559028550686adcc6
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2022-12-29 16:22:01 -0500

    added .config files for neovim and kitty term

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,25 @@
+local lsp = require('lsp-zero')
+
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = true,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = 'local',
+  sign_icons = {
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = ''
+  }
+})
+
+lsp.ensure_installed({
+	'sumneko_lua',
+})
+
+lsp.nvim_workspace()
+
+lsp.setup()
---
+++ .config/nvim/lua/bao/remap.lua
@@ -0,0 +1,2 @@
+-- Key bindings for fuzzy finder is located in after/plugin
+vim.keymap.set('n', '<leader>ii', ":Ex<Cr>", {})
