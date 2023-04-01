27f876db9aadc0c772bf52d2233513762232b94a
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-04-01 12:40:57 -0400

    updated whiteline spacing

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -5,29 +5,29 @@ vim.o.background = "dark" -- "dark" for dark mode or "light" for light mode
 -- vscode colorscheme
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
-  -- Alternatively set style in setup
-  -- style = 'light'
+    -- Alternatively set style in setup
+    -- style = 'light'
 
-  -- Enable transparent background
-  transparent = true,
+    -- Enable transparent background
+    transparent = true,
 
-  -- Enable italic comment
-  italic_comments = false,
+    -- Enable italic comment
+    italic_comments = false,
 
-  -- Disable nvim-tree background color
-  disable_nvimtree_bg = true,
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
 
-  -- Override colors (see ./lua/vscode/colors.lua)
-  color_overrides = {
-    vscLineNumber = '#FFFFFF',
-  },
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        vscLineNumber = '#FFFFFF',
+    },
 
-  -- Override highlight groups (see ./lua/vscode/theme.lua)
-  group_overrides = {
-    -- this supports the same val table as vim.api.nvim_set_hl
-    -- use colors from this colorscheme by requiring vscode.colors!
-    Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-  }
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
 })
 require('vscode').load()
 
@@ -40,19 +40,19 @@ require('vscode').load()
 
 -- transparency
 -- local function transparent_override()
-  --    local highlights = {
-    --      "Normal",
-    --      "LineNr",
-    --      "Folded",
-    --      "NonText",
-    --      "SpecialKey",
-    --      "VertSplit",
-    --      "SignColumn",
-    --      "EndOfBuffer",
-    --      "TablineFill"
-    --    }
-    --    for _, name in pairs(highlights) do
-    --      vim.cmd.highlight(name .. ' guibg=none ctermbg=none')
-    --    end
-    --  end
-    -- transparent_override()
+    --    local highlights = {
+        --      "Normal",
+        --      "LineNr",
+        --      "Folded",
+        --      "NonText",
+        --      "SpecialKey",
+        --      "VertSplit",
+        --      "SignColumn",
+        --      "EndOfBuffer",
+        --      "TablineFill"
+        --    }
+        --    for _, name in pairs(highlights) do
+        --      vim.cmd.highlight(name .. ' guibg=none ctermbg=none')
+        --    end
+        --  end
+        -- transparent_override()
