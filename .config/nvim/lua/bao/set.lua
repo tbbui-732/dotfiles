2c3ffd6bb93b0cf81ff71ba0cdb5d072551bac98
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-04-15 17:56:20 -0400

    set relative line number

--- .config/nvim/lua/bao/set.lua
+++ .config/nvim/lua/bao/set.lua
@@ -4,7 +4,7 @@ vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.opt.cursorline = true
 
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
