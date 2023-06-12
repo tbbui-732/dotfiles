7d92ef599360a0d7c1f8a9c6908766fd17bb66ed
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-06-12 08:26:50 -0400

    Update

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -2,33 +2,50 @@
 vim.o.background = "dark"
 
 -- Fluoromachine
-require('fluoromachine').setup {
-  glow = true,
-  theme = "delta",
-  --theme = "retrowave", 
-  --theme = "fluoromachine",  -- default
-  --transparent = "full",
-  overrides = {
-    ['@type'] =     { italic = true,  bold = false },
-    ['@function'] = { italic = false, bold = false },
-    ['@comment'] =  { italic = true },
-    ['@keyword'] =  { italic = false },
-    ['@constant'] = { italic = false, bold = true },
-    ['@variable'] = { italic = true },
-    ['@field'] =    { italic = true },
-    ['@parameter'] = { italic = true },
-  },
-  brightness = 0.03,
+--require('fluoromachine').setup {
+--  glow = true,
+--  theme = "delta",
+--  --theme = "retrowave", 
+--  --theme = "fluoromachine",  -- default
+--  --transparent = "full",
+--  overrides = {
+--    ['@type'] =     { italic = true,  bold = false },
+--    ['@function'] = { italic = false, bold = false },
+--    ['@comment'] =  { italic = true },
+--    ['@keyword'] =  { italic = false },
+--    ['@constant'] = { italic = false, bold = true },
+--    ['@variable'] = { italic = true },
+--    ['@field'] =    { italic = true },
+--    ['@parameter'] = { italic = true },
+--  },
+--  brightness = 0.03,
+--}
+--vim.cmd.colorscheme "fluoromachine"
+
+-- Sonokai
+-- vim.cmd("let g:sonokai_style = 'shusia'")
+-- vim.cmd("let g:sonokai_style_performance = '1'")
+-- vim.cmd("let g:sonokai_dim_inactive_windows = '1'")
+-- vim.cmd("let g:sonokai_enable_italic = '1'")
+-- vim.cmd.colorscheme "sonokai"
+
+-- Poimandres
+require('poimandres').setup {
+  bold_vert_split = false, -- use bold vertical separators
+  dim_nc_background = true, -- dim 'non-current' window backgrounds
+  disable_background = false, -- disable background
+  disable_float_background = false, -- disable background for floats
+  disable_italics = false, -- disable italics
 }
-vim.cmd.colorscheme "fluoromachine"
+vim.cmd.colorscheme 'poimandres'
 
 -- telescope highlighting settings
---local light_green = "#42675A"
---local sonokai_bg = "#212327"
---vim.api.nvim_set_hl(0, "TelescopeSelection",      { bg = light_green })
---vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = light_green })
---vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = sonokai_bg })
---vim.api.nvim_set_hl(0, "TelescopeTitle",  { bg = sonokai_bg })
+local light_green = "#42675A"
+local sonokai_bg = "#212327"
+vim.api.nvim_set_hl(0, "TelescopeSelection",      { bg = light_green })
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = light_green })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = sonokai_bg })
+vim.api.nvim_set_hl(0, "TelescopeTitle",  { bg = sonokai_bg })
 
 -- transparency
 -- local function transparent_override()
