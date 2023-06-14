e24284c72af4d7c64d4db8085804b6a3e1a91b08
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-06-14 08:52:02 -0400

    added config file for nvimtree

---
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -0,0 +1,22 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    side = "right",
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+-- key mappings
