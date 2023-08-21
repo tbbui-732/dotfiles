83135e147682c7c09b6d411d2a6415278441d3f2
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-08-20 21:02:03 -0400

    Update

--- .config/nvim/lua/bao/packer.lua
+++ .config/nvim/lua/bao/packer.lua
@@ -18,10 +18,10 @@ return require('packer').startup(function(use)
   }
 
   -- Colorschemes + Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require("nvim-treesitter.install").update { with_sync = true } end,
-  }
+  -- use {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   run = function() require("nvim-treesitter.install").update { with_sync = true } end,
+  -- }
   use { "ellisonleao/gruvbox.nvim" }
   use { "ishan9299/modus-theme-vim" }
 
