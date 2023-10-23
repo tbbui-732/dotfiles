a7222dcb533940c0a2afd8a8725a600ce9af7566
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-10-23 01:31:47 -0400

    feat: Update telescope to 0.1.4

--- .config/nvim/lua/bao/packer.lua
+++ .config/nvim/lua/bao/packer.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
 
   -- Navigation
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
