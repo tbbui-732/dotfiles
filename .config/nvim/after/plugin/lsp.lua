ff56b8dcb4292a65a407c1f14c7a45e36d105461
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-22 17:53:21 -0500

    Ctrl-S to get signature help

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -16,31 +16,30 @@ lsp.set_preferences({
         }
 })
 
--- Automatically displays diagnostics on hold
+-- Displays diagnostics on hold
 vim.api.nvim_create_autocmd({"CursorHold"}, {
-        callback = function()
-                vim.diagnostic.open_float()
-        end
+    callback = function()
+        vim.diagnostic.open_float()
+    end
 })
 
--- LSP mappings
+-- LSP actions
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
-lsp.nvim_workspace()
-lsp.setup()
-
---   -- LSP actions
---   map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<cr>')
+vim.keymap.set('i', '<c-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
 --   map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
 --   map('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
 --   map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
 --   map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
 --   map('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
 --   map('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
--- 
---   -- Diagnostics
---   map('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
---   map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
---   map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+-- Diagnostics
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+lsp.nvim_workspace()
+lsp.setup()
