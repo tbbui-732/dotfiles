c83399dce3903e8bf3a0c5c6c38262ea41fa7449
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-01-22 12:41:12 -0500

    nvim: autoclose brackets

--- .config/nvim/after/plugin/autoclose.lua
+++ .config/nvim/after/plugin/autoclose.lua
@@ -1,24 +1,28 @@
-local config = {
-   keys = {
-      ["("] = { escape = false, close = true, pair = "()" },
-      ["["] = { escape = false, close = true, pair = "[]" },
-      ["{"] = { escape = false, close = true, pair = "{}" },
-
-      [">"] = { escape = true, close = false, pair = "<>" },
-      [")"] = { escape = true, close = false, pair = "()" },
-      ["]"] = { escape = true, close = false, pair = "[]" },
-      ["}"] = { escape = true, close = false, pair = "{}" },
-   },
-   options = {
-      disabled_filetypes = { "text" },
-      disable_when_touch = false,
-      touch_regex = "[%w(%[{]",
-      pair_spaces = false,
-      auto_indent = true,
-      disable_command_mode = false,
-   },
-}
-
-require("autoclose").setup({
-    config
-})
+--local config = {
+--   keys = {
+--      ["("] = { escape = false, close = true, pair = "()" },
+--      ["["] = { escape = false, close = true, pair = "[]" },
+--      ["{"] = { escape = false, close = true, pair = "{}" },
+--
+--      [">"] = { escape = true, close = false, pair = "<>" },
+--      [")"] = { escape = true, close = false, pair = "()" },
+--      ["]"] = { escape = true, close = false, pair = "[]" },
+--      ["}"] = { escape = true, close = false, pair = "{}" },
+--
+--      ["\""] = { escape = false, close = false, pair = "\"" },
+--      ["'"]  = { escape = false, close = false, pair = "'" },
+--      ["`"]  = { escape = false, close = false, pair = "`" },
+--   },
+--   options = {
+--      disabled_filetypes = { "text" },
+--      disable_when_touch = false,
+--      touch_regex = "[%w(%[{]",
+--      pair_spaces = false,
+--      auto_indent = true,
+--      disable_command_mode = false,
+--   },
+--}
+--
+--require("autoclose").setup({
+--    config
+--})
